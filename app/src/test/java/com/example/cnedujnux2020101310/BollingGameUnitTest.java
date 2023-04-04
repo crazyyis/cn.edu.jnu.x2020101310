229a1d03b727922f9bed39290c96cbb1aa1d68cf
crazyyis <1529476025@qq.com>
2023-04-05 01:06:46 +0800

    调试test2

--- app/src/test/java/com/example/cnedujnux2020101310/BollingGameUnitTest.java
+++ app/src/test/java/com/example/cnedujnux2020101310/BollingGameUnitTest.java
@@ -11,9 +11,18 @@ import static org.junit.Assert.*;
  */
 public class BollingGameUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void test20zeros() {
         BollingGame game=new BollingGame();
         for(int i=0;i<20;i++)
+        {
+            game.roll(0);
+        }
+        assertEquals(0,game.score());
+    }
+    @Test
+    public void test20ones() {
+        BollingGame game=new BollingGame();
+        for(int i=1;i<20;i++)
         {
             game.roll(0);
         }
