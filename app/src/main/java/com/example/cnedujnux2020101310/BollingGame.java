f251c34bb2184c0ba013cc72d14aaee0f7aa8754
crazyyis <1529476025@qq.com>
2023-04-05 01:25:52 +0800

    完成test2

--- app/src/main/java/com/example/cnedujnux2020101310/BollingGame.java
+++ app/src/main/java/com/example/cnedujnux2020101310/BollingGame.java
@@ -1,10 +1,12 @@
 package com.example.cnedujnux2020101310;
 
 public class BollingGame {
+    int score=0;
     public void roll(int pins) {
+        score+=pins;
     }
 
     public int score() {
-        return -1;
+        return score;
     }
 }
--- app/src/test/java/com/example/cnedujnux2020101310/BollingGameUnitTest.java
+++ app/src/test/java/com/example/cnedujnux2020101310/BollingGameUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.cnedujnux2020101310;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,22 +11,29 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BollingGameUnitTest {
+
+    private BollingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BollingGame();
+    }
+
     @Test
     public void test20zeros() {
-        BollingGame game=new BollingGame();
-        for(int i=0;i<20;i++)
-        {
-            game.roll(0);
-        }
-        assertEquals(0,game.score());
+        repeatedRoll(0, 20);
+        assertEquals(0, game.score());
     }
     @Test
     public void test20ones() {
-        BollingGame game=new BollingGame();
-        for(int i=1;i<20;i++)
+        repeatedRoll(1,20);
+        assertEquals(20,game.score());
+    }
+
+    private void repeatedRoll(int pins, int times) {
+        for(int i=0;i<times;i++)
         {
-            game.roll(0);
+            game.roll(pins);
         }
-        assertEquals(0,game.score());
     }
 }
